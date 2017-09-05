285b5458d4e9c8860d057fffcb5e110badf2a854
ryotahirano <hirano@tsumikiinc.com>
2017-09-05 20:18:19 +0900

    fix test toContain to toMatch

--- test/InstagramGallery.test.js
+++ test/InstagramGallery.test.js
@@ -35,9 +35,9 @@ describe('Test suite for InstagramGallery', () => {
 
       const targetElChild = targetEl.querySelector('.c-instagram-gallery .c-instagram-item')
       expect(targetElChild).toBeTruthy()
-      expect(targetElChild.querySelector('img').src).toContain('dummy.jpg')
-      expect(targetElChild.querySelector('span').textContent).toContain('10 likes')
-      expect(targetElChild.querySelector('a').href).toContain('http://localhost/')
+      expect(targetElChild.querySelector('img').src).toMatch(/dummy.jpg/)
+      expect(targetElChild.querySelector('span').textContent).toMatch(/10 likes/)
+      expect(targetElChild.querySelector('a').href).toMatch(/http:\/\/localhost\//)
     })
   })
 
@@ -83,13 +83,13 @@ describe('Test suite for InstagramGallery', () => {
       const targetElChilds = targetEl.querySelectorAll('.c-instagram-gallery .c-instagram-item')
       expect(targetElChilds.length).toEqual(2)
 
-      expect(targetElChilds[0].querySelector('img').src).toContain('dummy.jpg')
-      expect(targetElChilds[0].querySelector('span').textContent).toContain('10 likes')
-      expect(targetElChilds[0].querySelector('a').href).toContain('http://localhost/')
+      expect(targetElChilds[0].querySelector('img').src).toMatch(/dummy.jpg/)
+      expect(targetElChilds[0].querySelector('span').textContent).toMatch(/10 likes/)
+      expect(targetElChilds[0].querySelector('a').href).toMatch(/http:\/\/localhost\//)
 
-      expect(targetElChilds[1].querySelector('img').src).toContain('dummy2.jpg')
-      expect(targetElChilds[1].querySelector('span').textContent).toContain('22 likes')
-      expect(targetElChilds[1].querySelector('a').href).toContain('http://localhost2/')
+      expect(targetElChilds[1].querySelector('img').src).toMatch(/dummy2.jpg/)
+      expect(targetElChilds[1].querySelector('span').textContent).toMatch(/22 likes/)
+      expect(targetElChilds[1].querySelector('a').href).toMatch(/http:\/\/localhost2\//)
     })
   })
 })
