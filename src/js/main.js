7cc04f919fd34d6eef9390aa64bc370288490dc0
ryotahirano <hirano@tsumikiinc.com>
2017-09-25 19:47:47 +0900

    eslint test

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@ import App from './components/App'
 import '../../node_modules/sanitize.css/sanitize.css'
 import '../css/style.scss'
 
+import './eslint-test'
+
 new Vue({
   el: '#app',
   template: '<App/>',
