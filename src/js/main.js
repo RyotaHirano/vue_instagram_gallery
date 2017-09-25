33442c99d394758b72680347b64792db047fac72
ryotahirano <hirano@tsumikiinc.com>
2017-09-25 19:49:38 +0900

    remove eslint test

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,6 @@ import App from './components/App'
 import '../../node_modules/sanitize.css/sanitize.css'
 import '../css/style.scss'
 
-import './eslint-test'
-
 new Vue({
   el: '#app',
   template: '<App/>',
